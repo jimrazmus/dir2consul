e2242ee526af690ae1338ee7523a2f58fd15901b
Jim Razmus II <jim.razmus@gmail.com>
2020-01-04 08:17:04 -0600

    Add IsEmpty testing.

--- kv_test.go
+++ kv_test.go
@@ -5,10 +5,21 @@ import (
 	"testing/quick"
 )
 
-func TestSet(t *testing.T) {
+func TestGetAndSet(t *testing.T) {
 	k := NewKVList()
 	err := quick.CheckEqual(k.Set, k.Get, nil)
 	if err != nil {
 		t.Error(err)
+	}
+}
+
+func TestIsEmpty(t *testing.T) {
+	k := NewKVList()
+	if k.IsEmpty() != true {
+		t.Error("New KVList should be empty")
+	}
+	_, _, _ = k.Set("foo", "bar")
+	if k.IsEmpty() == true {
+		t.Error("KVList should NOT be empty")
 	}
 }
