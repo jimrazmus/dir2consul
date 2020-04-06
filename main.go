eaa729284325fdbdcc82ec3d7c7af9077a29aa83
Dana McKiernan <dana.mckiernan@code42.com>
2020-04-06 14:22:51 -0500

    Added in, crudely, parsing for defaults and merging values

--- main.go
+++ main.go
@@ -10,6 +10,7 @@ import (
 	"regexp"
 	"sort"
 	"strings"
+	"errors"
 
 	"github.com/hashicorp/consul/api"
 	"github.com/jimrazmus/dir2consul/kv"
@@ -62,6 +63,7 @@ func main() {
 func setupEnvironment() {
 	viper.SetEnvPrefix("D2C")
 	viper.SetDefault("CONSUL_KEY_PREFIX", "dir2consul")
+	viper.SetDefault("DEFAULT_CONFIG_TYPE", "")
 	viper.SetDefault("DIRECTORY", "local/repo")
 	viper.SetDefault("DRYRUN", "false")
 	viper.SetDefault("IGNORE_DIR_REGEX", `a^`)
@@ -69,6 +71,7 @@ func setupEnvironment() {
 	viper.SetDefault("VERBOSE", "false")
 	viper.AutomaticEnv()
 	viper.BindEnv("CONSUL_KEY_PREFIX")
+	viper.BindEnv("DEFAULT_CONFIG_TYPE")
 	viper.BindEnv("DIRECTORY")
 	viper.BindEnv("DRYRUN")
 	viper.BindEnv("IGNORE_DIR_REGEX")
@@ -79,7 +82,7 @@ func setupEnvironment() {
 func startupMessage() string {
 	banner := "\n------------\n dir2consul \n------------\n"
 
-	config := "Configuration" + "\n\tD2C_CONSUL_KEY_PREFIX: " + viper.GetString("CONSUL_KEY_PREFIX") + "\n\tD2C_DIRECTORY: " + viper.GetString("DIRECTORY") + "\n\tD2C_DRYRUN: " + viper.GetString("DRYRUN") + "\n\tD2C_IGNORE_DIR_REGEX: " + viper.GetString("IGNORE_DIR_REGEX") + "\n\tD2C_IGNORE_FILE_REGEX: " + viper.GetString("IGNORE_FILE_REGEX") + "\n\tD2C_VERBOSE: " + viper.GetString("VERBOSE")
+	config := "Configuration" + "\n\tD2C_CONSUL_KEY_PREFIX: " + viper.GetString("CONSUL_KEY_PREFIX") + "\n\tD2C_DEFAULT_CONFIG_TYPE: " + viper.GetString("DEFAULT_CONFIG_TYPE") + "\n\tD2C_DIRECTORY: " + viper.GetString("DIRECTORY") + "\n\tD2C_DRYRUN: " + viper.GetString("DRYRUN") + "\n\tD2C_IGNORE_DIR_REGEX: " + viper.GetString("IGNORE_DIR_REGEX") + "\n\tD2C_IGNORE_FILE_REGEX: " + viper.GetString("IGNORE_FILE_REGEX") + "\n\tD2C_VERBOSE: " + viper.GetString("VERBOSE")
 
 	env := os.Environ()
 	sort.Strings(env)
@@ -147,43 +150,223 @@ func loadKeyValuesFromDisk(kv *kv.List, dirIgnoreRe *regexp.Regexp, fileIgnoreRe
 		elemKey := strings.TrimSuffix(path, filepath.Ext(path))
 
 		filetype := strings.TrimPrefix((strings.ToLower(filepath.Ext(path))), ".")
+		
+		if viper.GetBool("VERBOSE") {
+			log.Println("\n\n"+path+"\n" + elemKey + "\n")
+		}
+		
+		// Skip processing the root default file...
 
-		// Use the file extension to identify files that should undergo additional parsing
-		// and key-value creation.
-		switch filetype {
-		case "hcl", "ini", "json", "properties", "toml", "yaml", "yml":
-			// use a new Viper to parse the file contents
-			v := viper.NewWithOptions(viper.KeyDelimiter("/"))
-			v.SetConfigType(filetype)
-			f, err := os.Open(path)
+		// Are we looking at the root default file?
+                if path == "default" {
+			// Yup ... skipping
+		} else if info.Mode().IsDir() {
+			// Skip this, too -- we care about processing files, and only files
+		} else {
+		
+			// Find default files
+			pathPath := filepath.Dir(path)
+			pathRoot := viper.GetString("DIRECTORY")
+			defaultList, err := findDefaults(pathPath, pathRoot)
 			if err != nil {
+				log.Printf("Error processing path %s: %s", pathRoot + "/" + pathPath, err)
 				return err
 			}
-			err = v.ReadConfig(f)
-			if err != nil {
-				log.Printf("Error processing %s: %s", elemKey, err)
-				return nil
-			}
-			// iterate over keys within the file
-			for _, key := range v.AllKeys() {
-				if viper.GetBool("VERBOSE") {
-					log.Printf("%s=%s", elemKey+"/"+key, v.GetString(key))
+
+			pathFull := pathRoot + "/" + path
+			
+			// Add our path to the list, making it an ordered list of defaults + our file
+			// of interest
+			var filesToParse []string
+
+			filesToParse = append(filesToParse, defaultList...)
+
+			switch filetype {
+			case "hcl", "ini", "json", "properties", "toml", "yaml", "yml":
+				// If we understand the filetype, let Viper parse it...
+				if filepath.Base(path) != "default" {
+					filesToParse = append(filesToParse, pathFull)
 				}
-				kv.Set(viper.GetString("CONSUL_KEY_PREFIX")+"/"+elemKey+"/"+key, []byte(v.GetString(key)))
+
+				if  viper.GetBool("VERBOSE") {
+					for idx, p := range filesToParse {
+						log.Printf("    %d    %s", idx, p)
+					}
+				}
+
+				// Load & merge all the configuration files, in order
+				v, err := mergeConfiguration(filesToParse)
+				if err != nil {
+					log.Printf("Error merging configs! %s", err)
+					return err
+				}
+				
+
+				// iterate over keys within the file
+				for _, key := range v.AllKeys() {
+					if viper.GetBool("VERBOSE") {
+						log.Printf("%s=%s", elemKey+"/"+key, v.GetString(key))
+					}
+					kv.Set(viper.GetString("CONSUL_KEY_PREFIX")+"/"+elemKey+"/"+key, []byte(v.GetString(key)))
+				}
+			default:
+				// If we don't know the properties file type, load the whole thing as a blob... after the defaults.
+
+				// Revise that:  if we have a default type, load it now...
+				defaultType := viper.GetString("DEFAULT_CONFIG_TYPE")
+
+				if defaultType != "" {
+					if filepath.Base(path) != "default" {
+						filesToParse = append(filesToParse, pathFull)
+					}
+				}
+
+				
+				
+				if  viper.GetBool("VERBOSE") {
+					for idx, p := range filesToParse {
+						log.Printf("+++ %d    %s", idx, p)
+					}
+				}
+
+				// Load & merge all the configuration files, in order
+				v, err := mergeConfiguration(filesToParse)
+				if err != nil {
+					log.Printf("Error merging configs! %s", err)
+					return err
+				}
+				
+
+				// iterate over keys within the file
+				for _, key := range v.AllKeys() {
+					if viper.GetBool("VERBOSE") {
+						log.Printf("%s=%s", elemKey+"/"+key, v.GetString(key))
+					}
+					kv.Set(viper.GetString("CONSUL_KEY_PREFIX")+"/"+elemKey+"/"+key, []byte(v.GetString(key)))
+				}
+
+				if defaultType == "" {
+					// Now that the default files are absorbed, absorb this whole file as a single property.
+					if info.Size() > 512000 {
+						log.Printf("Skipping %s: size exceeds Consul's 512KB limit", elemKey)
+					}
+					
+					elemVal, err := ioutil.ReadFile(path)
+					if err != nil {
+						return err
+					}
+					if viper.GetBool("VERBOSE") {
+						log.Printf("%s=%s", elemKey, []byte(elemVal))
+					}
+					kv.Set(viper.GetString("CONSUL_KEY_PREFIX")+"/"+elemKey, elemVal)
+				}
 			}
-		default:
-			if info.Size() > 512000 {
-				log.Printf("Skipping %s: size exceeds Consul's 512KB limit", elemKey)
-				return nil
-			}
-			elemVal, err := ioutil.ReadFile(path)
-			if err != nil {
-				return err
-			}
-			if viper.GetBool("VERBOSE") {
-				log.Printf("%s=%s", elemKey, []byte(elemVal))
-			}
-			kv.Set(viper.GetString("CONSUL_KEY_PREFIX")+"/"+elemKey, elemVal)
+			// // Use the file extension to identify files that should undergo additional parsing
+			// // and key-value creation.
+			// switch filetype {
+			// case "hcl", "ini", "json", "properties", "toml", "yaml", "yml":
+			// 	// Find default files
+			// 	pathPath := filepath.Dir(path)
+			// 	pathRoot := viper.GetString("DIRECTORY")
+			// 	defaultList, err := findDefaults(pathPath, pathRoot)
+			// 	if err != nil {
+			// 		log.Printf("Error processing path %s: %s", pathRoot + "/" + pathPath, err)
+			// 		return err
+			// 	}
+
+			// 	pathFull := pathRoot + "/" + path
+				
+			// 	// Add our path to the list, making it an ordered list of defaults + our file
+			// 	// of interest
+			// 	var filesToParse []string
+
+			// 	filesToParse = append(filesToParse, defaultList...)
+			// 	filesToParse = append(filesToParse, pathFull)
+
+			// 	if  viper.GetBool("VERBOSE") {
+			// 		for idx, p := range filesToParse {
+			// 			log.Printf("    %d    %s", idx, p)
+			// 		}
+			// 	}
+
+			// 	// Load & merge all the configuration files, in order
+			// 	v, err := mergeConfiguration(filesToParse)
+			// 	if err != nil {
+			// 		log.Printf("Error merging configs! %s", err)
+			// 		return err
+			// 	}
+				
+			// 	// use a new Viper to parse the file contents
+			// 	// v := viper.NewWithOptions(viper.KeyDelimiter("/"))
+			// 	// v.SetConfigType(filetype)
+			// 	// f, err := os.Open(path)
+			// 	// if err != nil {
+			// 	// 	return err
+			// 	// }
+				
+			// 	// err = v.ReadConfig(f)
+			// 	// if err != nil {
+			// 	// 	log.Printf("Error processing %s: %s", elemKey, err)
+			// 	// 	return nil
+			// 	// }
+				
+			// 	// iterate over keys within the file
+			// 	for _, key := range v.AllKeys() {
+			// 		if viper.GetBool("VERBOSE") {
+			// 			log.Printf("%s=%s", elemKey+"/"+key, v.GetString(key))
+			// 		}
+			// 		kv.Set(viper.GetString("CONSUL_KEY_PREFIX")+"/"+elemKey+"/"+key, []byte(v.GetString(key)))
+			// 	}
+			// default:
+			// 	if info.Size() > 512000 {
+			// 		log.Printf("Skipping %s: size exceeds Consul's 512KB limit", elemKey)
+			// 		return nil
+			// 	}
+			// 	// Find default files
+			// 	pathPath := filepath.Dir(path)
+			// 	pathRoot := viper.GetString("DIRECTORY")
+			// 	defaultList, err := findDefaults(pathPath, pathRoot)
+			// 	if err != nil {
+			// 		log.Printf("Error processing path %s: %s", pathRoot + "/" + pathPath, err)
+			// 		return err
+			// 	}
+
+			// 	pathFull := pathRoot + "/" + path
+				
+			// 	// Add our path to the list, making it an ordered list of defaults + our file
+			// 	// of interest
+			// 	var filesToParse []string
+
+			// 	filesToParse = append(filesToParse, defaultList...)
+			// 	filesToParse = append(filesToParse, pathFull)
+
+			// 	if  viper.GetBool("VERBOSE") {
+			// 		for idx, p := range filesToParse {
+			// 			log.Printf("    %d    %s", idx, p)
+			// 		}
+			// 	}
+				
+			// 	// Load & merge all the configuration files, in order
+			// 	v, err := mergeConfiguration(filesToParse)
+			// 	if err != nil {
+			// 		log.Printf("Error merging configs! %s", err)
+			// 		return err
+			// 	}
+			// 	// elemVal, err := ioutil.ReadFile(path)
+			// 	// if err != nil {
+			// 	// 	return err
+			// 	// }
+			// 	// if viper.GetBool("VERBOSE") {
+			// 	// 	log.Printf("%s=%s", elemKey, []byte(elemVal))
+			// 	// }
+			// 	// kv.Set(viper.GetString("CONSUL_KEY_PREFIX")+"/"+elemKey, elemVal)
+			// 	for _, key := range v.AllKeys() {
+			// 		if viper.GetBool("VERBOSE") {
+			// 			log.Printf("%s=%s", elemKey+"/"+key, v.GetString(key))
+			// 		}
+			// 		kv.Set(viper.GetString("CONSUL_KEY_PREFIX")+"/"+elemKey+"/"+key, []byte(v.GetString(key)))
+			// 	}
+			// }
 		}
 
 		return nil
@@ -228,6 +411,193 @@ func deleteExtraConsulData(fileKeyValues *kv.List, consulKeyValues *kv.List, con
 			}
 		}
 	}
+}
+
+func findDefaults(path string, root string) ([]string, error) {
+	var results []string
+	
+	fullPath := root + "/" + path
+	
+	fullPathInfo, err := os.Stat(fullPath)
+	if os.IsNotExist(err) {
+		// Our path doesn't exist
+		return nil, err
+	} else {
+		if fullPathInfo.IsDir() {
+			
+			rootDefault := root + "/default"
+			
+			rootPathInfo, err := os.Stat(rootDefault)
+			if os.IsNotExist(err){
+				// Our root default does not exist, do nothing
+			} else {
+				// Our root default does exist, add it to the results
+				if ! rootPathInfo.IsDir() {
+					results = append(results, rootDefault)
+				} else {
+					// our root path is a file, somehow
+				}
+			}
+			
+			dirElements := strings.Split(path, "/")
+			
+			var dirConcat string
+			
+			for idx, a := range dirElements {
+				// Iterate over path elements, looking for a `default` file in each one...
+				if idx == 0 {
+					dirConcat = a
+				} else {
+					dirConcat = dirConcat + "/" + a
+				}
+				
+				// Full path of the element we're currently at
+				aPath := root + "/" + dirConcat
+				
+				// Does this exist?  it really should, but worse things happen at sea...
+				aPathInfo, err := os.Stat(aPath)
+				if os.IsNotExist(err) {
+					// You should never get here
+					return nil, err
+				} else {
+					if aPathInfo.IsDir() {
+						aPathDefault := aPath + "/default"
+						
+						_, err = os.Stat(aPathDefault)
+						if os.IsNotExist(err) {
+							// a defualt does not exist, do nothing
+						} else {
+							// A default *has* been found, add it to the list
+							results = append(results, aPathDefault)
+						}
+					} else {
+						// We found a file, not a directory.  You should never get here
+					}
+				}
+				
+			} // end of range
+			
+		} else {
+			err := errors.New("findDefaults called with file instead of path")
+			return nil, err
+		}
+	}
+	
+	return results, nil
+}
+
+func mergeConfiguration(files []string) (config *viper.Viper, err error) {
+	
+	// Make an array of viper objects
+	var viperList []viper.Viper
+	
+	// Make a viper object to hold the merged config
+	zfinal := viper.New()
+	
+	for _, z := range files {
+		
+		// For each file in our list, read it into a new viper object
+		// zv := viper.New()
+		
+		// zDir  := filepath.Dir(z)
+		// zFile := filepath.Base(z)
+		
+		// zv.SetConfigName(zFile)
+		// zv.AddConfigPath(zDir)
+		// zv.SetConfigType("properties")
+		
+		// err := zv.ReadInConfig()
+		zv, err := loadFile(z)
+		
+		if err != nil {
+			return nil, fmt.Errorf("Fatal error config file %s: %s\n", z, err)
+		}
+		
+		// Add this viper object to our array of viper objects
+		viperList = append(viperList, *zv)
+		
+		zvSettings := zv.AllSettings()
+		
+		// Merge in the settings in the newly loaded viper object into our
+		// merged viper object
+		err = zfinal.MergeConfigMap(zvSettings)
+		if err != nil {
+			return nil, fmt.Errorf("Unable to merge configuration! %s", err)
+		}
+	}
+	
+	return zfinal, nil
+}    
+
+func loadFile (path string) (*viper.Viper, error) {
+	results := viper.New()
+	
+	elemKey := strings.TrimSuffix(path, filepath.Ext(path))
+	filetype := strings.TrimPrefix((strings.ToLower(filepath.Ext(path))), ".")
+	
+	defaultType := viper.GetString("DEFAULT_CONFIG_TYPE")
+	
+	
+	switch filetype {
+	case "hcl", "ini", "json", "properties", "toml", "yaml", "yml":
+		fileDir := filepath.Dir(path)
+		fileFile := filepath.Base(path)
+		
+		results.SetConfigName(fileFile)
+		results.AddConfigPath(fileDir)
+		results.SetConfigType(filetype)
+		
+		err := results.ReadInConfig()
+		if err != nil {
+			return nil, fmt.Errorf("Fatal error config file %s: %s\n", path, err)
+		}
+	default:
+		if defaultType == "" {
+			// We already have this from earlier in the program.  It would behoove us to figure
+			// out how to get it down to here in the callstack w/out restat-ing the file...
+			aFile, err := os.Open(path)
+			if err != nil {
+				log.Printf("Unable to open file %s: %s", path, err)
+				return nil, err
+			}
+			
+			info, err := aFile.Stat()
+			if err != nil {
+				log.Printf("Error stat-ing file: %s", err)
+				return nil, err
+			}
+
+			if info.Size() > 512000 {
+				log.Printf("Skipping %s: size exceeds Consul's 512KB limit", elemKey)
+				return nil, fmt.Errorf("Skipping %s: size exceeds Consul's 512KB limit", elemKey)
+			}
+			
+			elemVal, err := ioutil.ReadFile(path)
+			if err != nil {
+				return nil, err
+			}
+			if viper.GetBool("VERBOSE") {
+				log.Printf("%s=%s", elemKey, []byte(elemVal))
+			}
+			// kv.Set(viper.GetString("CONSUL_KEY_PREFIX")+"/"+elemKey, elemVal)
+			results.Set(elemKey, elemVal)
+			// panic(fmt.Sprintf("No default config type, trying to load a file in loadfile..."))
+		} else {
+			fileDir := filepath.Dir(path)
+			fileFile := filepath.Base(path)
+			
+			results.SetConfigName(fileFile)
+			results.AddConfigPath(fileDir)
+			results.SetConfigType(defaultType)
+			
+			err := results.ReadInConfig()
+			if err != nil {
+				return nil, fmt.Errorf("Fatal error config file %s: %s\n", path, err)
+			}
+		}
+	}
+	
+	return results, nil
 }
 
 // Emacs formatting variables
